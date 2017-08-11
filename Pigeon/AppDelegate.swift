fb34eb3ac0153983afcf6c8ec27ab6c1c5ca74cf
ffreylaw <ffreylaw@gmail.com>
2017-08-11 18:59:17 +1000

    update appdelegate

--- Pigeon/AppDelegate.swift
+++ Pigeon/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.tintColor = .black
+        window?.rootViewController = ViewController()
+        
         return true
     }
 
