73f660a2390d93f1cabd4f456bcaa01c40a18e15
ssswww4444 <peiyuns@student.unimelb.edu.au>
2017-09-04 14:56:00 +1000

    chats

---
+++ Pigeon/Chats/NewChatTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  NewChatTableViewCell.swift
+//  Pigeon
+//
+//  Created by Pei Yun Sun on 2017/9/4.
+//  Copyright © 2017 El Root. All rights reserved.
+//
+
+import UIKit
+
+class NewChatTableViewCell: UITableViewCell {
+    
+    var user: User? {
+        didSet {
+            setupUser()
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupUser() {
+        nameLabel.text = user?.name
+        usernameLabel.text = user?.username
+        if let url = user?.profilePhotoURL {
+            profilePhotoImageView.loadImageUsingCache(with: url)
+        }
+    }
+    
+    fileprivate func setupViews() {
+        addSubview(nameLabel)
+        addSubview(usernameLabel)
+        addSubview(profilePhotoImageView)
+        
+        profilePhotoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
+        profilePhotoImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
+        profilePhotoImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profilePhotoImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: profilePhotoImageView.rightAnchor, constant: 16).isActive = true
+        
+        usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4).isActive = true
+        usernameLabel.leftAnchor.constraint(equalTo: nameLabel.leftAnchor).isActive = true
+        
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.sizeToFit()
+        return label
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.sizeToFit()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
+        return label
+    }()
+    
+    let profilePhotoImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = linePixel
+        imageView.layer.borderColor = lineColor.cgColor
+        return imageView
+    }()
+    
+}
