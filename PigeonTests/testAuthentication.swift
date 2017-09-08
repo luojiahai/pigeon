990399e6f635b8b1d3e826af8b4c133cbf1192bf
ffreylaw <ffreylaw@gmail.com>
2017-09-08 11:27:42 +1000

    test auth updated

--- PigeonTests/testAuthentication.swift
+++ PigeonTests/testAuthentication.swift
@@ -1,5 +1,5 @@
 //
-//  testLogin.swift
+//  testAuthentication.swift
 //  PigeonTests
 //
 //  Created by Meng Yuan on 6/9/17.
@@ -11,12 +11,14 @@ import Firebase
 
 @testable import Pigeon
 
-class testLogin: XCTestCase {
+class testAuthentication: XCTestCase {
+    
+    var loginVC: LoginViewController!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+        loginVC = Pigeon.LoginViewController(nibName: "Login", bundle: Bundle.main)
     }
     
     override func tearDown() {
@@ -36,9 +38,28 @@ class testLogin: XCTestCase {
         }
     }
     
-    func testEmail() {
+    func testEmailPassword() {
         //should be false
+        let loginView = loginVC.loginView
         
+        XCTAssertNotNil(loginView, "view not loaded")
+
+        loginView.emailTextField.text = "demo@gmail.com"
+        loginView.passwordTextField.text = "123123"
+        
+        let email = loginView.emailTextField.text
+        let password = loginView.passwordTextField.text
+        
+        XCTAssertNotNil(email, "email field is nil")
+        XCTAssertNotNil(password, "password field is nil")
+        
+        loginVC.loginToDatabase(email: email!, password: password!)
+        
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { (timer) in
+            let user = Auth.auth().currentUser
+            
+            XCTAssertNotNil(user, "login failed")
+        }
     }
     
 }
