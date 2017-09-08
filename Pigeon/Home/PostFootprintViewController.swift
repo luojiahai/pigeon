44a282167b4ea7642c29600894ef67f4b99b2d07
ffreylaw <ffreylaw@gmail.com>
2017-09-09 00:13:07 +1000

    add photos for post footprint

--- Pigeon/Home/PostFootprintViewController.swift
+++ Pigeon/Home/PostFootprintViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class PostFootprintViewController: UIViewController {
+    
+    var addImageButtonConstraint: NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +19,7 @@ class PostFootprintViewController: UIViewController {
         setupNavigation()
     }
 
-    func setupNavigation() {
+    fileprivate func setupNavigation() {
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.tintColor = .black
         navigationItem.title = "Post Footprint"
@@ -25,24 +27,22 @@ class PostFootprintViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(handleDone))
     }
     
-    @objc func handleCancel() {
-//        if let caption = captionTextView.text, caption != "" || captionImageView.image != nil {
-//            let alert = UIAlertController(title: "Warning", message: "Will lost all filled data", preferredStyle: UIAlertControllerStyle.alert)
-//            alert.addAction(UIAlertAction(title: "Leave", style: UIAlertActionStyle.default, handler: {action in self.dismiss(animated: true, completion: nil)}))
-//            alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
-//            present(alert, animated: true, completion: nil)
-//        } else {
-//            dismiss(animated: true, completion: nil)
-//        }
-        
-        dismiss(animated: true, completion: nil)
+    @objc fileprivate func handleCancel() {
+        if let caption = captionTextView.text, caption != "" || captionImageViews.count > 0 {
+            let alert = UIAlertController(title: "Warning", message: "Will lost all filled data\nAre you sure want to quit?", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: {action in self.dismiss(animated: true, completion: nil)}))
+            alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else {
+            dismiss(animated: true, completion: nil)
+        }
     }
     
-    @objc func handleDone() {
+    @objc fileprivate func handleDone() {
         
     }
     
-    func setupViews() {
+    fileprivate func setupViews() {
         view.backgroundColor = .groupTableViewBackground
         
         view.addSubview(captionContainerView)
@@ -53,16 +53,67 @@ class PostFootprintViewController: UIViewController {
         
         captionContainerView.addSubview(captionTextView)
         
-        captionTextView.topAnchor.constraint(equalTo: captionContainerView.topAnchor, constant: 4).isActive = true
-        captionTextView.bottomAnchor.constraint(equalTo: captionContainerView.bottomAnchor, constant: -4).isActive = true
-        captionTextView.leftAnchor.constraint(equalTo: captionContainerView.leftAnchor, constant: 4).isActive = true
-        captionTextView.rightAnchor.constraint(equalTo: captionContainerView.rightAnchor, constant: -4).isActive = true
+        captionTextView.topAnchor.constraint(equalTo: captionContainerView.topAnchor, constant: 8).isActive = true
+        captionTextView.bottomAnchor.constraint(equalTo: captionContainerView.bottomAnchor, constant: -8).isActive = true
+        captionTextView.leftAnchor.constraint(equalTo: captionContainerView.leftAnchor, constant: 8).isActive = true
+        captionTextView.rightAnchor.constraint(equalTo: captionContainerView.rightAnchor, constant: -8).isActive = true
         
-        captionContainerView.addSubview(seperatorLine)
-        seperatorLine.bottomAnchor.constraint(equalTo: captionContainerView.bottomAnchor).isActive = true
-        seperatorLine.leftAnchor.constraint(equalTo: captionContainerView.leftAnchor).isActive = true
-        seperatorLine.rightAnchor.constraint(equalTo: captionContainerView.rightAnchor).isActive = true
+        view.addSubview(imageContainerView)
+        imageContainerView.topAnchor.constraint(equalTo: captionContainerView.bottomAnchor).isActive = true
+        imageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        imageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        imageContainerView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        imageContainerView.addSubview(addImageButton)
+        addImageButtonConstraint = addImageButton.leftAnchor.constraint(equalTo: imageContainerView.leftAnchor, constant: 8)
+        addImageButtonConstraint?.isActive = true
+        addImageButton.topAnchor.constraint(equalTo: imageContainerView.topAnchor, constant: 8).isActive = true
+        addImageButton.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor, constant: -8).isActive = true
+        addImageButton.widthAnchor.constraint(equalTo: addImageButton.heightAnchor).isActive = true
+        
+        imageContainerView.addSubview(seperatorLine)
+        seperatorLine.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor).isActive = true
+        seperatorLine.leftAnchor.constraint(equalTo: imageContainerView.leftAnchor).isActive = true
+        seperatorLine.rightAnchor.constraint(equalTo: imageContainerView.rightAnchor).isActive = true
         seperatorLine.heightAnchor.constraint(equalToConstant: linePixel).isActive = true
+    }
+    
+    fileprivate func addCaptionImageView(_ image: UIImage) {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = linePixel
+        imageView.layer.borderColor = lineColor.cgColor
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDeleteImage)))
+        captionImageViews.append(imageView)
+        
+        var lastImageViewRightAnchor: NSLayoutXAxisAnchor?
+        captionImageViews.forEach { (imageView) in
+            imageContainerView.addSubview(imageView)
+            if let lastAnchor = lastImageViewRightAnchor {
+                imageView.leftAnchor.constraint(equalTo: lastAnchor, constant: 8).isActive = true
+            } else {
+                imageView.leftAnchor.constraint(equalTo: imageContainerView.leftAnchor, constant: 8).isActive = true
+            }
+            imageView.topAnchor.constraint(equalTo: imageContainerView.topAnchor, constant: 8).isActive = true
+            imageView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor, constant: -8).isActive = true
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor).isActive = true
+            lastImageViewRightAnchor = imageView.rightAnchor
+        }
+        
+        if captionImageViews.count >= 3 {
+            addImageButton.isHidden = true
+        } else {
+            addImageButtonConstraint?.constant += 92
+        }
+    }
+    
+    fileprivate func deleteCaptionImageView(_ imageView: UIImageView) {
+        let alert = UIAlertController(title: "Delete Photo", message: "Feature coming soon...", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     let captionContainerView: UIView = {
@@ -86,60 +137,83 @@ class PostFootprintViewController: UIViewController {
         return view
     }()
     
+    let imageContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    lazy var addImageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("addPhoto", for: .normal)
+        button.setTitleColor(lineColor, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        button.layer.borderWidth = linePixel
+        button.layer.borderColor = lineColor.cgColor
+        button.addTarget(self, action: #selector(handleAddImage), for: .touchUpInside)
+        return button
+    }()
+    
+    var captionImageViews = [UIImageView]()
 }
 
 extension PostFootprintViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @objc fileprivate func handleAddImage() {
-//        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-//
-//        alert.addAction(UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in self.openCamera() })
-//        alert.addAction(UIAlertAction(title: "Photos", style: .default) { (UIAlertAction) in self.openPhotoLibrary() })
-//        if captionImageView.image != nil {
-//            alert.addAction(UIAlertAction(title: "Delete Photo", style: UIAlertActionStyle.destructive, handler: { (UIAlertAction) in self.captionImageView.image = nil}))
-//        }
-//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-//        present(alert, animated: true, completion: nil)
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default) { (UIAlertAction) in self.openCamera() })
+        alert.addAction(UIAlertAction(title: "Photos", style: .default) { (UIAlertAction) in self.openPhotoLibrary() })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
-//    fileprivate func openCamera() {
-//        let picker = UIImagePickerController()
-//
-//        picker.delegate = self
-//        picker.sourceType = UIImagePickerControllerSourceType.camera
-//        picker.allowsEditing = true
-//
-//        present(picker, animated: true, completion: nil)
-//    }
-//
-//    fileprivate func openPhotoLibrary() {
-//        let picker = UIImagePickerController()
-//
-//        picker.delegate = self
-//        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-//        picker.allowsEditing = true
-//
-//        present(picker, animated: true, completion: nil)
-//    }
-//
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//        var selectedImageFromPicker: UIImage?
-//
-//        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
-//            selectedImageFromPicker = editedImage
-//        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
-//            selectedImageFromPicker = originalImage
-//        }
-//
-//        if let selectedImage = selectedImageFromPicker {
-//            captionImageView.image = selectedImage
-//        }
-//
-//        dismiss(animated: true, completion: nil)
-//    }
-//
-//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//        dismiss(animated: true, completion: nil)
-//    }
+    @objc fileprivate func handleDeleteImage(_ sender: UIImageView) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Delete Photo", style: .destructive) { (UIAlertAction) in self.deleteCaptionImageView(sender) })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    fileprivate func openCamera() {
+        let picker = UIImagePickerController()
+
+        picker.delegate = self
+        picker.sourceType = UIImagePickerControllerSourceType.camera
+        picker.allowsEditing = true
+
+        present(picker, animated: true, completion: nil)
+    }
+
+    fileprivate func openPhotoLibrary() {
+        let picker = UIImagePickerController()
+
+        picker.delegate = self
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        picker.allowsEditing = true
+
+        present(picker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var selectedImageFromPicker: UIImage?
+
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+
+        if let selectedImage = selectedImageFromPicker {
+            addCaptionImageView(selectedImage)
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     
 }
