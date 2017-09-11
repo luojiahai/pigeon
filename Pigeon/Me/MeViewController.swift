dd7abadf46080f33e80ff9ab959457a97bda0846
ffreylaw <ffreylaw@gmail.com>
2017-09-11 10:58:58 +1000

    footprints on me view done

--- Pigeon/Me/MeViewController.swift
+++ Pigeon/Me/MeViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 import Firebase
 
-class MeViewController: UIViewController {
+class MeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .groupTableViewBackground
+        collectionView.alwaysBounceVertical = true
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(FootprintCollectionViewCell.self, forCellWithReuseIdentifier: "FootprintCell")
+        return collectionView
+    }()
+    
+    var refreshControl: UIRefreshControl?
+    
+    var footprints = [Footprint]()
     
     var completion: (() -> Void)?
     
@@ -19,6 +35,7 @@ class MeViewController: UIViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
         fetchUser()
+        fetchFootprintsForMe()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -44,6 +61,16 @@ class MeViewController: UIViewController {
     
     fileprivate func supportViews() {
         meView.editProfileButton.addTarget(self, action: #selector(handleEditProfile), for: .touchUpInside)
+        
+        meView.addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: meView.nameCardView.bottomAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: meView.bottomAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: meView.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: meView.rightAnchor).isActive = true
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(reloadData), for: UIControlEvents.valueChanged)
+        collectionView.addSubview(refreshControl!)
     }
     
     fileprivate func fetchUser() {
@@ -81,15 +108,95 @@ class MeViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    fileprivate func fetchFootprintsForMe() {
+        guard let currentUser = Auth.auth().currentUser else { return }
+        
+        Database.database().reference().child("footprints").queryOrdered(byChild: "timestamp").observeSingleEvent(of: .value) { (dataSnapshot) in
+            guard let objects = dataSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for object in objects {
+                guard let uid = object.childSnapshot(forPath: "user").value as? String else { return }
+                if currentUser.uid != uid {
+                    continue
+                }
+                Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (userDataSnapshot) in
+                    guard let userDictionary = userDataSnapshot.value as? [String: AnyObject] else { return }
+                    let user = User(uid: uid, userDictionary)
+                    let footprint = Footprint()
+                    footprint.user = user
+                    footprint.text = object.childSnapshot(forPath: "text").value as? String
+                    footprint.timestamp = object.childSnapshot(forPath: "timestamp").value as? NSNumber
+                    if object.hasChild("images") {
+                        footprint.imageURLs = [String]()
+                        guard let imageURLs = object.childSnapshot(forPath: "images").children.allObjects as? [DataSnapshot] else { return }
+                        for imageURL in imageURLs {
+                            let image = imageURL.value as? String
+                            footprint.imageURLs?.append(image!)
+                        }
+                    }
+                    guard let location = object.childSnapshot(forPath: "location").value as? [String: Any] else { return }
+                    footprint.latitude = location["latitude"] as? Double
+                    footprint.longitude = location["longitude"] as? Double
+                    footprint.altitude = location["altitude"] as? Double
+                    
+                    self.footprints.insert(footprint, at: 0)
+                    
+                    DispatchQueue.main.async(execute: {
+                        self.collectionView.reloadData()
+                    })
+                })
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return footprints.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FootprintCell", for: indexPath)
+        
+        if let cell = cell as? FootprintCollectionViewCell {
+            cell.footprint = footprints[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 16, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let footprintVC = FootprintViewController()
+        footprintVC.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(footprintVC, animated: true)
+    }
+    
 }
 
 // MARK: - LoginViewControllerDelegate
 // MeViewController is a delegate for LoginViewController. 
 // It provides the functionality of cleaning and reloading data in the HomeViewController itself.
 extension MeViewController: LoginViewControllerDelegate {
-    func reloadData() {
+    
+    @objc func reloadData() {
         fetchUser()
+        
+        footprints.removeAll()
+        collectionView.reloadData()
+        fetchFootprintsForMe()
+        
+        refreshControl?.endRefreshing()
     }
+    
 }
 
 extension MeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
