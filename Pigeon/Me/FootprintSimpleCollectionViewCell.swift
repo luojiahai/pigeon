65d8010517b2cbfd2a147718ba8eb756506d7684
ffreylaw <ffreylaw@gmail.com>
2017-09-16 13:12:04 +1000

    minor UI layout fixed

--- Pigeon/Me/FootprintSimpleCollectionViewCell.swift
+++ Pigeon/Me/FootprintSimpleCollectionViewCell.swift
@@ -103,6 +103,7 @@ class FootprintSimpleCollectionViewCell: UICollectionViewCell {
         footprintTextLabel.heightAnchor.constraint(equalToConstant: 32).isActive = true
         
         footprintLocationLabel.bottomAnchor.constraint(equalTo: numLikesCommentsLabel.topAnchor, constant: -6).isActive = true
+        footprintLocationLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
         
         timeLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
         timeLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
