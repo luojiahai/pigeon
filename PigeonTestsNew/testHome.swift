4ba058c1c263d66ea13574c6a6a5a4091fc1356f
Abby Yuan <abigail_yuan@hotmail.com>
2017-10-11 19:12:21 +1100

    Home view tests passed

--- PigeonTestsNew/testHome.swift
+++ PigeonTestsNew/testHome.swift
@@ -13,19 +13,31 @@ import Firebase
 
 class testHome: XCTestCase {
     
+    var homeVC: HomeViewController!
+    var loginVC:LoginViewController!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        loginVC = Pigeon.LoginViewController(nibName: "Login", bundle: Bundle.main)
+        loginVC.loginToDatabase(email: "abigail_yuan@hotmail.com", password: "yuanmeng960426")
+        homeVC = HomeViewController(collectionViewLayout: UICollectionViewFlowLayout())
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        homeVC = nil
         super.tearDown()
     }
     
-    func testExample() {
+    func testFootprintsTable() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(loginVC != nil, "login view not loaded")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(4), execute: {
+            XCTAssert(self.homeVC.footprints.count != 0, "footprints not loaded") //should not be 0
+        })
     }
     
     func testPerformanceExample() {
