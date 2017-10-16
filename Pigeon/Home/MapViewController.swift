d1539c840b20ab532dfff21714853347312993d8
TinaLuan <tinaluan9794@gmail.com>
2017-10-16 15:46:55 +1100

    fixed dundun's map

--- Pigeon/Home/MapViewController.swift
+++ Pigeon/Home/MapViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Pei Yun Sun on 2017/9/5.
 //  Copyright © 2017 El Root. All rights reserved.
 //
+
 import UIKit
 import MapKit
 import CoreLocation
@@ -27,7 +28,9 @@ class MapViewController: UIViewController {
     var user: User?
     
     var footprints: [Footprint]?
-
+    
+    var routeShown: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +38,47 @@ class MapViewController: UIViewController {
         setupViews()
         setupLocationManager()
         setupMapView()
+        renderFootprint()
+    }
+    
+    @objc fileprivate func handleAnnotationTap(_ gesture: UITapGestureRecognizer) {
+        print("Annotation Tapped")
+        let marker = gesture.view as? MKMarkerAnnotationView
+        let annotation = marker?.annotation
+        let vc = FootprintopoverViewController()
+        for footprint in footprints! {
+            // find the footprint of the annotaiton with footprintID
+            if footprint.footprintID == (annotation?.subtitle)! {
+                vc.footprint = footprint // pass the footprint
+            }
+        }
+        
+        // setup the popover controller
+        vc.modalPresentationStyle = UIModalPresentationStyle.popover
+        vc.preferredContentSize = CGSize(width: 256, height: 256)
+        vc.popoverPresentationController?.sourceView = view
+        vc.popoverPresentationController?.sourceRect = CGRect(x: (view.frame.width - 256)/2, y: (view.frame.height - 256)/2, width: 256, height: 256)
+        vc.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.init(rawValue: 0)
+        vc.popoverPresentationController?.delegate = self
+        
+        present(vc, animated: true, completion: nil)
+        
+    }
+    
+    fileprivate func renderFootprint() {
+        if footprints == nil {
+            return
+        }
+        
+        // render each footprint
+        for footprint in footprints! {
+            let coordinate = CLLocationCoordinate2D(latitude: footprint.latitude!, longitude: footprint.longitude!)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = footprint.user?.name
+            annotation.subtitle = footprint.footprintID
+            mapView.addAnnotation(annotation)
+        }
     }
     
     fileprivate func setupNavigation() {
@@ -76,6 +120,60 @@ class MapViewController: UIViewController {
             repeats: true)
     }
     
+    func setRoute() {
+        // get coordinates
+        let sourceCoordinate = currentLocation?.coordinate
+        let destCoordinate = targetLocation?.coordinate
+        
+        // create Placemarks
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceCoordinate!)
+        let destPlaceMark = MKPlacemark(coordinate: destCoordinate!)
+        
+        // create MapItems
+        let sourceItem = MKMapItem(placemark: sourcePlaceMark)  // POI on map
+        let destItem = MKMapItem(placemark: destPlaceMark)
+        
+        // Name the MapItems
+        sourceItem.name = "Source"
+        destItem.name = "Destination"
+        
+        // Create a direction request
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceItem
+        directionRequest.destination = destItem
+        directionRequest.transportType = .any  // can modify transport type
+        let directions = MKDirections(request: directionRequest) // computes directions and travel time
+        
+        // Find direction and draw route
+        directions.calculate(completionHandler: {
+            response, error in
+            
+            // check response
+            guard let response = response else {
+                if error != nil {
+                    print("Error during calculation of directions")
+                }
+                return
+            }
+            
+            // draw route
+            let route = response.routes[0]  // 0 for the fastest route
+            self.mapView.add(route.polyline, level: .aboveRoads)
+            
+            //            // set region
+            //            let rect = route.polyline.boundingMapRect
+            //            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        })
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        // return a renderer for rendering polyline of the route
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .blue  // color of the polyline
+        renderer.lineWidth = 5.0
+        return renderer
+    }
+    
     @objc func updateUserLocation() {
         if let currentLocation = currentLocation {
             DispatchQueue.main.async {
@@ -95,7 +193,7 @@ class MapViewController: UIViewController {
                         _ in
                         self.mapView.region.span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
                     })
-                }   
+                }
             }
         }
         
@@ -118,6 +216,15 @@ class MapViewController: UIViewController {
                 self.targetLocation = CLLocation(coordinate: CLLocationCoordinate2D(latitude: dictionary["latitude"]!, longitude: dictionary["longitude"]!), altitude: dictionary["altitude"]!)
             })
         }
+        
+        // set the route from current location to target location
+        if currentLocation != nil &&
+            targetLocation != nil &&
+            !routeShown {
+            setRoute()
+            routeShown = true
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -137,6 +244,7 @@ class MapViewController: UIViewController {
         let arVC = ARViewController()
         arVC.delegate = self
         arVC.targetLocation = targetLocation
+        arVC.footprints = footprints
         let vc = UINavigationController(rootViewController: arVC)
         present(vc, animated: false, completion: nil)
     }
@@ -200,7 +308,13 @@ extension MapViewController: MKMapViewDelegate, CLLocationManagerDelegate {
                 marker.markerTintColor = UIColor(hue: 0.267, saturation: 0.67, brightness: 0.77, alpha: 1.0)
                 marker.glyphImage = UIImage(named: "compass")
             } else {
-                // footprints maybe...
+                // footprints
+                marker.displayPriority = .required
+                marker.markerTintColor = .gray
+                marker.glyphImage = UIImage(named: "icons8-Cat Footprint Filled-50")
+                let gesture = UITapGestureRecognizer(target: self, action: #selector(handleAnnotationTap))
+                marker.addGestureRecognizer(gesture)
+                
             }
             
             return marker
@@ -222,6 +336,15 @@ extension MapViewController: ARViewControllerDelegate {
     }
     
 }
+
+extension MapViewController: UIPopoverPresentationControllerDelegate {
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
+    
+}
+
 
 extension MapViewController: LocationSharingStatusListener {
     func dismissMap() {
